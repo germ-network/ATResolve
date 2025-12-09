763b044997059fb42694f1897cd0a3b85b690d11
Mark Xue <mark@germ.network>
2025-12-09 13:34:33 -0800

    remove AsyncHTTPClient package dependency

--- Package.swift
+++ Package.swift
@@ -19,22 +19,20 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-async-dns-resolver.git", from: "0.4.0"),
-		.package(url: "https://github.com/swift-server/async-http-client.git", from: "1.30.0"),
-		.package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
 	],
 	targets: [
 		.target(
 			name: "ATResolve",
 			dependencies: [
-				.product(name: "AsyncDNSResolver", package: "swift-async-dns-resolver"),
-			]),
+				.product(
+					name: "AsyncDNSResolver",
+					package: "swift-async-dns-resolver"
+				),
+			]
+		),
 		.testTarget(
 			name: "ATResolveTests",
-			dependencies: [
-				"ATResolve",
-				.product(name: "AsyncHTTPClient", package: "async-http-client"),
-				.product(name: "NIOHTTP1", package: "swift-nio")
-			]
+			dependencies: ["ATResolve"]
 		),
 	]
 )
