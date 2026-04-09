90325340a2d5bd7ff802dd7b67d7f199789b39f3
Anna M <anna@germ.network>
2026-04-08 17:53:53 -0700

    Get rid of generic

--- Sources/ATResolve/ATResolver.swift
+++ Sources/ATResolve/ATResolver.swift
@@ -33,7 +33,7 @@ public struct PLCDirectoryResolveDidResponse: Codable, Hashable, Sendable {
 	}
 }
 
-public struct ATResolver<Provider: HTTPFetcher> {
+public struct ATResolver {
 	public let provider: HTTPFetcher
 
 	public init(provider: HTTPFetcher) {
